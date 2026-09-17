1ce23df811bc342e4d0c65a1ee96046d92cc9666
xiaolajyc <72300443+xiaolajyc@users.noreply.github.com>
2026-09-17 20:39:39 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='todo-calendar-v2-103';
+const CACHE='todo-calendar-v2-105';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
